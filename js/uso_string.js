16f15494d1ec7b089b3e8fb387594edf060a3298
ChrisFL234 <fleytasisaac@gmail.com>
2025-01-22 19:36:30 -0300

    comilla doble y enter

--- js/uso_string.js
+++ js/uso_string.js
@@ -1,8 +1,12 @@
 const comillasimple ='comilla simple'
 const comilladoble = "comilla doble"
+const comilladoblecomilladoble = "comilla \"doble\"";
+const soyunenter = "soy un \nenter";
 //template string
 const tmp =`voy a construir un parrafo utilizando las otras dos constantes de arriba. Estas son ${comillasimple} y ${comilladoble}`;
 const chamburreado = "a veces me siento fotosintesis"
 const chamburreado2 = 'epa, puchero'
 console.log(tmp);
 console.log(chamburreado);
+console.log(comilladoblecomilladoble);
+console.log(soyunenter);
