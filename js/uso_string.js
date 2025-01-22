a7588bb60250c85e776045a1ab54aedb0d741849
ChrisFL234 <fleytasisaac@gmail.com>
2025-01-22 19:29:12 -0300

    ahora si imprimen las ${}

--- js/uso_string.js
+++ js/uso_string.js
@@ -1,7 +1,7 @@
 const comillasimple ='comilla simple'
 const comilladoble = "comilla doble"
 //template string
-const tmp = 'voy a construir un parrafo utilizando las otras dos constantes de arriba. Estas son ${comillasimple} y ${comilladoble}';
+const tmp =`voy a construir un parrafo utilizando las otras dos constantes de arriba. Estas son ${comillasimple} y ${comilladoble}`;
 const chamburreado = "a veces me siento fotosintesis"
 const chamburreado2 = 'epa, puchero'
 console.log(tmp);
