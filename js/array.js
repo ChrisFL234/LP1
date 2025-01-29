ab801bfe1bc94a342250bd687f9a09aae8b8851f
ChrisFL234 <fleytasisaac@gmail.com>
2025-01-29 19:37:27 -0300

    uso de arrays

--- js/array.js
+++ js/array.js
@@ -1,2 +1,31 @@
 //zona segura 
-document.addEventListener("DOMContentLoaded", function(){});
+document.addEventListener("DOMContentLoaded", function(){
+const frutas = ["pomelo", "naranja","manzana"];
+const verduras = ["tomate", "locote","lechuga"];
+const vero = {
+    nombre: "veronica belen"
+    , apellido: "vera irala"
+    , edad: null
+};
+const ale = {
+    nombre: "alexis"
+    , apellido: "aguilera"
+    , edad: 29
+};
+const jorge = {
+    nombre: "jorge"
+    , apellido: "acuña"
+};
+
+const personas = [vero, ale, jorge];
+
+//length es el numero de elementos
+//para encontra un elemento conocido
+
+console.log (frutas.length);
+console.log (verduras[verduras.length -1]);
+console.log (personas);
+frutas.push("pomelo");
+
+
+});
